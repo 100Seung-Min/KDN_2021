5371f2fdc98472c08845ce832b46554adebbea56
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2021-09-08 23:50:02 +0900

    9월 8일

--- Health_Food/app/src/main/java/com/example/health_food/Fragment/Eat.kt
+++ Health_Food/app/src/main/java/com/example/health_food/Fragment/Eat.kt
@@ -10,79 +10,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.core.widget.addTextChangedListener
+import androidx.recyclerview.widget.RecyclerView
 import com.example.health_food.R
 import com.example.health_food.model.Food
 
 class Eat : Fragment() {
-    val foodsearch: ArrayList<Food> = ArrayList()
-    var foodlist:ArrayList<Food> = ArrayList()
-//    val listviewadapter = ListViewAdapter(activity!!, foodsearch)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        foodsearch.add(Food(fooditem = "채수빈"))
-        foodsearch.add(Food(fooditem = "채수빈"))
-        foodsearch.add(Food(fooditem = "채수빈"))
-        foodsearch.add(Food(fooditem = "채수빈"))
-        foodsearch.add(Food(fooditem = "채수빈"))
-        foodsearch.add(Food(fooditem = "채수빈"))
-        foodsearch.add(Food(fooditem = "채수빈"))
-        foodsearch.add(Food(fooditem = "채수빈"))
-        foodlist.addAll(foodsearch)
-//        view?.findViewById<ListView>(R.id.search_food_lisst)?.adapter = listviewadapter
-        view?.findViewById<EditText>(R.id.search_food_edit)?.addTextChangedListener (object : TextWatcher{
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                TODO("Not yet implemented")
-            }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-            override fun afterTextChanged(s: Editable?) {
-                val text = view?.findViewById<EditText>(R.id.search_food_edit)?.text.toString()
-//            search(text)
-                println("여기" + text)
-                Toast.makeText(activity, text, Toast.LENGTH_SHORT).show()
-            }
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                TODO("Not yet implemented")
-            }
-        })
         return inflater.inflate(R.layout.fragment_eat, container, false)
     }
-//    private fun search(text:String){
-//        foodsearch.clear()
-//        if (text.length == 0) {
-//            foodsearch.addAll(foodlist)
-//        }
-//        else{
-//            for(i in 0..foodlist.size - 1 step (1)){
-//                if(foodlist.get(i).toString().contains(text)){
-//                    foodsearch.add(foodlist.get(i))
-//                }
-//            }
-//        }
-//        listviewadapter.notifyDataSetChanged()
-//    }
-//    inner class ListViewAdapter(val context: Context, val food: ArrayList<Food>): BaseAdapter(){
-//        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-//            val view: View = LayoutInflater.from(context).inflate(R.layout.item_food, null)
-//            val foodItem = view.findViewById<TextView>(R.id.item_food_txt)
-//            val foodname = food[position]
-//            foodItem.text = foodname.fooditem
-//            return view
-//        }
-//
-//        override fun getItem(position: Int): Any {
-//            return food[position]
-//        }
-//
-//        override fun getItemId(position: Int): Long {
-//            return 0
-//        }
-//
-//        override fun getCount(): Int {
-//            return food.size
-//        }
-//    }
 }
--- Health_Food/app/src/main/java/com/example/health_food/Fragment/Search.kt
+++ Health_Food/app/src/main/java/com/example/health_food/Fragment/Search.kt
@@ -1,21 +1,92 @@
 package com.example.health_food.Fragment
 
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
 import com.example.health_food.R
+import com.example.health_food.model.Food
 
 class Search : Fragment() {
+    val foodsearch: ArrayList<Food> = ArrayList()
+    var foodlist:ArrayList<Food> = ArrayList()
+    lateinit var listviewadapter:ListViewAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        println("안녕")
+        foodsearch.add(Food(fooditem = "채수빈"))
+        foodsearch.add(Food(fooditem = "채수빈"))
+        foodsearch.add(Food(fooditem = "채수빈"))
+        foodsearch.add(Food(fooditem = "채수빈"))
+        foodsearch.add(Food(fooditem = "채수빈"))
+        foodlist.addAll(foodsearch)
+        listviewadapter = ListViewAdapter(foodsearch)
+        view?.findViewById<ListView>(R.id.search_food_lisst)?.adapter = listviewadapter
+        view?.findViewById<EditText>(R.id.search_food_edit)?.addTextChangedListener (object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                val text = view?.findViewById<EditText>(R.id.search_food_edit)?.text.toString()
+                search(text)
+                println("여기" + text)
+                Toast.makeText(activity, text, Toast.LENGTH_SHORT).show()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                val text = view?.findViewById<EditText>(R.id.search_food_edit)?.text.toString()
+                search(text)
+                println("여기" + text)
+                Toast.makeText(activity, text, Toast.LENGTH_SHORT).show()
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val text = view?.findViewById<EditText>(R.id.search_food_edit)?.text.toString()
+                search(text)
+                println("여기" + text)
+                Toast.makeText(activity, text, Toast.LENGTH_SHORT).show()
+            }
+        })
         return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+    private fun search(text:String){
+        foodsearch.clear()
+        if (text.length == 0) {
+            foodsearch.addAll(foodlist)
+        }
+        else{
+            for(i in 0..foodlist.size - 1 step (1)){
+                if(foodlist.get(i).fooditem.contains(text)){
+                    foodsearch.add(foodlist.get(i))
+                }
+            }
+        }
+        listviewadapter.notifyDataSetChanged()
+    }
+    inner class ListViewAdapter(val food: ArrayList<Food>): BaseAdapter(){
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            val view: View = LayoutInflater.from(context).inflate(R.layout.item_food, null)
+            val foodItem = view.findViewById<TextView>(R.id.item_food_txt)
+            val foodname = food[position].fooditem
+            foodItem.text = foodname
+            return view
+        }
+
+        override fun getItem(position: Int): Any {
+            return food[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getCount(): Int {
+            return food.size
+        }
     }
 }
