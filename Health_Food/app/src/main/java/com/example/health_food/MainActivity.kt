e01aea51f6dd4b90025f65c3225ff7f75dc1d8ea
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2021-09-18 20:35:22 +0900

    9월 18일

--- Health_Food/app/src/main/java/com/example/health_food/MainActivity.kt
+++ Health_Food/app/src/main/java/com/example/health_food/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.example.health_food
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
-import com.example.health_food.Fragment.Diary
-import com.example.health_food.Fragment.Eat
-import com.example.health_food.Fragment.MainFragment
-import com.example.health_food.Fragment.Search
+import com.example.health_food.Fragment.*
 import com.example.health_food.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,20 +20,36 @@ class MainActivity : AppCompatActivity() {
 
         ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1)
         replaceFrgment(MainFragment())
+        val toolbar = mbinding.sideNav
+        setSupportActionBar(toolbar)
+        val actionbar = supportActionBar
 
+
+        actionbar!!.setDisplayShowCustomEnabled(true)
+        actionbar!!.setDisplayShowTitleEnabled(false)
+        actionbar!!.setDisplayHomeAsUpEnabled(true)
         mbinding.navigation.itemIconTintList = null
+        mbinding.navigation.selectedItemId = R.id.home
         mbinding.navigation.setOnNavigationItemSelectedListener { item ->
-            when(item.itemId){
+            when(item.itemId) {
                 R.id.home -> {
+                    mbinding.sideNav.visibility = View.VISIBLE
                     replaceFrgment(MainFragment())
                     return@setOnNavigationItemSelectedListener true
                 }
                 R.id.diary -> {
+                    mbinding.sideNav.visibility = View.GONE
                     replaceFrgment(Diary())
                     return@setOnNavigationItemSelectedListener true
                 }
                 R.id.search -> {
+                    mbinding.sideNav.visibility = View.GONE
                     replaceFrgment(Search())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.userprofile -> {
+                    mbinding.sideNav.visibility = View.GONE
+                    replaceFrgment(UserProfile())
                     return@setOnNavigationItemSelectedListener true
                 }
                 else -> {
@@ -45,5 +63,26 @@ class MainActivity : AppCompatActivity() {
         val fragmentTransaction = supportFragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.frame_1, fragment)
         fragmentTransaction.commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.sidemenu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.side_health -> {
+                Toast.makeText(this, "건강식 선택", Toast.LENGTH_SHORT).show()
+            }
+            R.id.side_local -> {
+                Toast.makeText(this, "일반식 선택", Toast.LENGTH_SHORT).show()
+            }
+            R.id.side_say -> {
+                startActivity(Intent(this, Complain::class.java))
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
