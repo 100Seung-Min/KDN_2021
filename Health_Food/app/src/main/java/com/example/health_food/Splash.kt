090f572ac35f42f8b028d3f56c40567382c3cfa3
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2021-09-19 22:43:41 +0900

    9월 19일

--- Health_Food/app/src/main/java/com/example/health_food/Splash.kt
+++ Health_Food/app/src/main/java/com/example/health_food/Splash.kt
@@ -1,15 +1,20 @@
 package com.example.health_food
 
+import android.content.Intent
 import android.os.Bundle
-import android.support.wearable.activity.WearableActivity
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 
-class Splash : WearableActivity() {
+class Splash : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        // Enables Always-on
-        setAmbientEnabled()
+        val handler = Handler()
+        handler.postDelayed({
+            startActivity(Intent(this, StartActivity::class.java))
+            finish()
+        }, 2000)
     }
 }
