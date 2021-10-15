6be296a8d7333200c703c20c1cbe85769daed6bb
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2021-10-15 22:17:31 +0900

    10월 15일

--- Health_Food/app/src/main/java/com/example/health_food/Login.kt
+++ Health_Food/app/src/main/java/com/example/health_food/Login.kt
@@ -3,21 +3,47 @@ package com.example.health_food
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.health_food.databinding.FragmentLoginBinding
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.FacebookSdk
+import com.facebook.appevents.AppEventsLogger
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.kakao.sdk.auth.LoginClient
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.util.Utility
 import com.kakao.sdk.common.model.AuthErrorCause.*
+import com.kakao.sdk.user.UserApiClient
+import java.util.*
 
 class Login: AppCompatActivity() {
 
     val binding by lazy { FragmentLoginBinding.inflate(layoutInflater)}
+    var callbackManager: CallbackManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        callbackManager = CallbackManager.Factory.create()
+        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+            if (token != null) {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }
+        }
+        binding.kakaoLoginBtn.setOnClickListener {
+            if(LoginClient.instance.isKakaoTalkLoginAvailable(this)){
+                LoginClient.instance.loginWithKakaoTalk(this, callback = callback)
+            }else{
+                LoginClient.instance.loginWithKakaoAccount(this, callback = callback)
+            }
+        }
         binding.loginBtn.setOnClickListener {
             if(binding.emailEdit.text!!.isNullOrEmpty() || binding.pwEdit.text!!.isNullOrEmpty()){
                 Toast.makeText(this, "두개 다 입력해주세요", Toast.LENGTH_SHORT).show()
@@ -34,6 +60,37 @@ class Login: AppCompatActivity() {
         binding.findPassword.setOnClickListener {
             startActivity(Intent(this, Findpassword::class.java))
             finish()
+        }
+        binding.facebookLoginBtn.setOnClickListener {
+            LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile", "email"))
+            LoginManager.getInstance().registerCallback(callbackManager, object :
+                FacebookCallback<LoginResult> {
+                override fun onSuccess(loginResult: LoginResult) {
+                }
+
+                override fun onCancel() {
+                }
+
+                override fun onError(error: FacebookException) {
+                }
+            })
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackManager?.onActivityResult(requestCode, resultCode, data)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        UserApiClient.instance.accessTokenInfo { tokenInfo, error ->
+            if (error != null) {
+            }
+            else if (tokenInfo != null) {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }
         }
     }
 }
